ee06afaa817dd4a552c0ab9361bd6082e4531113
Raghu Betina <rbetina@github.com>
2015-03-06 09:56:31 -0600

    It works

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 
+gem 'koala'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304154420) do
+ActiveRecord::Schema.define(version: 20150306155227) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.string   "facebook_access_token"
   end
 
 end
